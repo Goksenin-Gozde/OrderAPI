1baebdbcbb7f0fb21bca5f154db09d04c0d8fe77
Göksenin Gözde <goksenin.gozde@krontech.com>
2023-02-03 01:17:11 +0300

    fix: unnecessary lines deleted from test

--- src/test/java/com/coffe/order/OrderApiApplicationTests.java
+++ src/test/java/com/coffe/order/OrderApiApplicationTests.java
@@ -1,10 +1,7 @@
 package com.coffe.order;
 
 import org.junit.jupiter.api.Test;
-import org.modelmapper.ModelMapper;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootTest
@@ -14,15 +11,6 @@ class OrderApiApplicationTests {
     @Test
     void contextLoads() {
 
-    }
-
-    @TestConfiguration
-    public static class TestConfig {
-
-        @Bean
-        public ModelMapper modelMapper() {
-            return new ModelMapper();
-        }
     }
 
 }
